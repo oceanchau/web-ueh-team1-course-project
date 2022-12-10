ed1f5eda7efbecf6e4c6a6818f80adc78b41b019
Danny-98 <ducdang1206@gmail.com>
2022-12-09 19:07:25 -0800

    add components

---
+++ admin/view/components/admin-pagination.php
@@ -0,0 +1,33 @@
+<?php
+$pageNumber = getParams("pageNumber");
+$limit = getParams("limit");
+
+$initial_pages = ($pageNumber) * $limit;
+$total_pages = ceil(200 / $limit);
+
+$isPreDisable = $pageNumber == 0 ? "disabled" : "";
+$isNextDisable = $pageNumber == $total_pages - 1 ? "disabled" : "";
+?>
+<nav aria-label="...">
+    <ul class="pagination">
+        <li class="page-item <?php echo $isPreDisable; ?>">
+            <a class="page-link" href="<?php echo getHyperLink($pages, $pageNumber - 1, $limit); ?>"
+                tabindex="-1">Previous</a>
+        </li>
+        <?php
+        for ($i = 0; $i < $total_pages; $i++) {
+            $isActive = $pageNumber == $i ? "active" : "inactive";
+        ?>
+
+        <li class="page-item <?php echo $isActive; ?>">
+            <a class="page-link" href="<?php echo getHyperLink($pages, $i, $limit); ?>">
+                <?php echo $i; ?>
+            </a>
+        </li>
+
+        <?php } ?>
+        <li class="page-item <?php echo $isNextDisable; ?>">
+            <a class="page-link" href="<?php echo getHyperLink($pages, $pageNumber + 1, $limit); ?>">Next</a>
+        </li>
+    </ul>
+</nav>
---
+++ admin/view/components/modal/add-product-modal.php
@@ -0,0 +1,24 @@
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <form action="./?p=products&pageNumber=1&limit=20" method="post">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Add Product</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+
+                    <div class="form-outline form-white mb-4">
+                        <input type="email" id="typeEmailX" name="email" class="form-control form-control-lg" />
+                        <label class="form-label" for="typeEmailX">Email</label>
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
